720e9ef4aa45f4ed3fc9f6eec3d95aa96ffeae09
LiuHao <1505932002@qq.com>
2019-04-12 11:06:08 +0800

    test

---
+++ STL/STL/2jjalloc.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STL
+//
+//  Created by 刘昊 on 2019/4/12.
+//  Copyright © 2019 刘昊. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ STL/STL/2jjalloc.h
@@ -0,0 +1,13 @@
+//
+//  2jjalloc.h
+//  STL
+//
+//  Created by 刘昊 on 2019/4/12.
+//  Copyright © 2019 刘昊. All rights reserved.
+//
+
+#ifndef _jjalloc_h
+#define _jjalloc_h
+
+
+#endif /* _jjalloc_h */
